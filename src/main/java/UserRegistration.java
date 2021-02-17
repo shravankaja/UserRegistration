c4f670779a5df026eb29245cbfa73699f13e4720
shravan-kaja <shravan.kaja@gmail.com>
2021-02-17 22:14:29 +0530

    Added: added method to check if entered last name is valid

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -3,24 +3,39 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+    static boolean value;
 
     public static void isValidFirstName(String firstName) {
 
         Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}$");
         Matcher matcher = pattern.matcher(firstName);
-        if (matcher.find()) {
+        value = matcher.find();
+        validCheck(value);
+    }
+
+    public static void isValidLastName(String lastName) {
+
+        Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}$");
+        Matcher matcher = pattern.matcher(lastName);
+        value = matcher.find();
+        validCheck(value);
+    }
+
+    public static void validCheck(boolean value) {
+        if (value) {
             System.out.println("Valid");
         } else {
             System.out.println("Invalid");
         }
-
     }
 
     public static void main(String Args[]) {
-        System.out.println("Enter First Name");
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first name \n");
         String firstName = sc.next();
         isValidFirstName(firstName);
-
+        System.out.println("Enter last name \n");
+        String lastName = sc.next();
+        isValidFirstName(lastName);
     }
 }
