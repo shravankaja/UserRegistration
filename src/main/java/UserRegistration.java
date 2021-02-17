61fe160d70b013fbd639e9790f37eff7c8741933
shravan-kaja <shravan.kaja@gmail.com>
2021-02-17 22:01:41 +0530

    Added: added method to check if entered first name is valid

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,5 +1,26 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+
+    public static void isValidFirstName(String firstName) {
+
+        Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}$");
+        Matcher matcher = pattern.matcher(firstName);
+        if (matcher.find()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+
+    }
+
     public static void main(String Args[]) {
-        System.out.println("Welcome to User Registration");
+        System.out.println("Enter First Name");
+        Scanner sc = new Scanner(System.in);
+        String firstName = sc.next();
+        isValidFirstName(firstName);
+
     }
 }
