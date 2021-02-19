33078be2c269c399db73dce26f0899ae16670117
shravan-kaja <shravan.kaja@gmail.com>
2021-02-19 11:26:40 +0530

    Checked: ensured given emails passed

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -1,76 +1,91 @@
+package com.userregistration;
+
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    static boolean value;
+    boolean value;
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String phone;
+    public String password;
 
-    public static void isValidFirstName(String firstName) {
+    public String isValidFirstName(String firstName) {
 
         Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}$");
         Matcher matcher = pattern.matcher(firstName);
         value = matcher.find();
-        validCheck(value);
+        return validCheck(value);
+
     }
 
-    public static void isValidLastName(String lastName) {
+    public String isValidLastName(String lastName) {
 
         Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}$");
         Matcher matcher = pattern.matcher(lastName);
         value = matcher.find();
-        validCheck(value);
+        return validCheck(value);
+
     }
 
-    public static void isValidEmail(String email) {
+    public String isValidEmail(String email) {
 
         Pattern pattern = Pattern.compile(
                 "^[a-zA-z0-9]{1,}([\\.\\_\\+\\-])?[a-zA-Z0-9]{0,}@[a-zA-z0-9]{1,}\\.[a-z]{2,3}(\\.)?([a-z]{2,3})?");
         Matcher matcher = pattern.matcher(email);
         value = matcher.find();
-        validCheck(value);
+        return validCheck(value);
+
     }
 
-    public static void isValidPhone(String phone) {
+    public String isValidPhone(String phone) {
 
         String regexPhone = "^[9][1][0-9]{10}$";
         Pattern pattern = Pattern.compile(regexPhone);
         Matcher matcher = pattern.matcher(phone);
         value = matcher.matches();
-        validCheck(value);
+        return validCheck(value);
 
     }
-    public static void isValidPassword(String password) {
+
+    public String isValidPassword(String password) {
 
         Pattern pattern = Pattern.compile("^(?=.{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!@#$%^&*])(?!.*[!@#$%^&*].*[!@#$%^&*]).*$");
         Matcher matcher = pattern.matcher(password);
         value = matcher.find();
-        validCheck(value);
+        return validCheck(value);
+
     }
 
-    public static void validCheck(boolean value) {
+    public String validCheck(boolean value) {
         if (value) {
             System.out.println("Valid");
+            return "Valid";
         } else {
             System.out.println("Invalid");
+            return "Invalid";
         }
     }
 
     public static void main(String Args[]) {
         Scanner sc = new Scanner(System.in);
+        UserRegistration userRegistration = new UserRegistration();
         System.out.println("Enter first name \n");
-        String firstName = sc.next();
-        isValidFirstName(firstName);
+        userRegistration.firstName = sc.next();
+        userRegistration.isValidFirstName(userRegistration.firstName);
         System.out.println("Enter last name \n");
-        String lastName = sc.next();
-        isValidFirstName(lastName);
+        userRegistration.lastName = sc.next();
+        userRegistration.isValidFirstName(userRegistration.lastName);
         System.out.println("Enter Email \n");
-        String email = sc.next();
-        isValidEmail(email);
+        userRegistration.email = sc.next();
+        userRegistration.isValidEmail(userRegistration.email);
         System.out.println("Enter phone \n");
-        String phone = sc.next();
-        isValidPhone(phone);
+        userRegistration.phone = sc.next();
+        userRegistration.isValidPhone(userRegistration.phone);
         System.out.println("Enter password \n");
-        String password = sc.next();
-        isValidPassword(password);
+        userRegistration.password = sc.next();
+        userRegistration.isValidPassword(userRegistration.password);
     }
 }
---
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -0,0 +1,42 @@
+package com.userregistration;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+
+public class UserRegistrationTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"Shrvaan.kaja@gmiil.com", "abc.xyz@gmail.com"})
+    public void emailTest(String email) throws Exception {
+        UserRegistration userRegistration = new UserRegistration();
+        Assertions.assertEquals("Valid", userRegistration.isValidEmail(email));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Shrvaankaja", "DghDui"})
+    public void firstNameTest(String name) throws Exception {
+        UserRegistration userRegistration = new UserRegistration();
+        Assertions.assertEquals("Valid", userRegistration.isValidFirstName(name));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Shrvaankaja", "DghDui"})
+    public void lastNameTest(String name) throws Exception {
+        UserRegistration userRegistration = new UserRegistration();
+        Assertions.assertEquals("Valid", userRegistration.isValidLastName(name));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"919740216522", "919999999999"})
+    public void phoneTest(String phone) throws Exception {
+        UserRegistration userRegistration = new UserRegistration();
+        Assertions.assertEquals("Valid", userRegistration.isValidPhone(phone));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"shravK24$sasa", "Ksddq23%dasd"})
+    public void passwordTest(String password) throws Exception {
+        UserRegistration userRegistration = new UserRegistration();
+        Assertions.assertEquals("Valid", userRegistration.isValidP(password));
+    }
+}
